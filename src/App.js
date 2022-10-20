d7ef13f8df0c510d900f74a0950ab562f43ae1b0
Lisa Yang <lisayang041@gmail.com>
2022-10-20 21:55:01 +0800

    Installed Unicons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react';
 
 function App() {
   return (
     <div className="App">
 <h1 className='flex align-middle underline'>Hello</h1>
+    <UilReact/>
     </div>
   );
 }
